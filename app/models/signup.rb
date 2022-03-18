72561576e20bb7ee5dfa9df22d89e556c7eb910d
Felipa Mendez <felipalouise@gmail.com>
2022-03-18 11:22:31 -0500

    commit tree 802ab7d83f504cb6fee81ef6be22c015b8eb4656

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -2,6 +2,8 @@ class Signup < ApplicationRecord
   belongs_to :activity
   belongs_to :camper
 
-  validates :time, numericality: { greater_than_or_equal_to: 0 }
-  validates :time, numericality: { less_than_or_equal_to: 23 }
+
+  validates :time, inclusion: 0..23
+  # validates :time, numericality: { greater_than_or_equal_to: 0 }
+  # validates :time, numericality: { less_than_or_equal_to: 23 }
 end
