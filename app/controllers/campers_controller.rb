3fb195ca01bc9bf7b54d12b7cafd3ba1a0a7159e
Felipa Mendez <felipalouise@gmail.com>
2022-03-18 10:07:25 -0500

    done with 2 errors

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,36 @@
+class CampersController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unproccessable_entity
+
+
+    def index
+        campers = Camper.all 
+        render json: campers, status: :ok
+    end
+
+    def show
+      camper = Camper.find(params[:id])
+      render json: camper, status: :ok, serializer:CamperActivitySerializer
+    end
+
+    def create
+        camper = Camper.create!(camper_params)
+        render json: camper, status: :created
+     end
+
+    private
+
+    def render_not_found_response
+        render json: {error: "Camper not found"}, status: :not_found
+    end
+
+   def camper_params
+        params.permit(:name, :age)
+   end
+
+    def render_unproccessable_entity(invalid)
+        render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+    end
+
+end
